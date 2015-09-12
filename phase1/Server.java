b77512cbee9e442fd269fd4455ad52c52b0e407c
GrantMoyer <grantmoyer@gmail.com>
2015-09-12 18:50:18 -0400

    Now Server Compiles

--- phase1/Server.java
+++ phase1/Server.java
@@ -1,7 +1,10 @@
-import java.net.DatagramSocket
-import java.net.DatagramPacket
-import java.net.InetSocketAddress
-import java.util.Vector
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Vector;
 
 /**
  * Sends a file to the client
@@ -9,12 +12,13 @@ import java.util.Vector
 public class Server {
     public static void main() {
         // Make sure server exits cleanly
-        Runtime.getRuntime().addShutdownHook(new Thread(new ExitManager()))
+        Runtime.getRuntime().addShutdownHook(new Thread(new ExitManager()));
 
         // Start the server
         ServerRunnable serve = new ServerRunnable();
         Thread serverThread = new Thread(serve);
         serverThread.start();
+        System.out.println("Server Started");
 
     }
 }
@@ -22,8 +26,8 @@ public class Server {
 /**
  * Manages exiting cleanly
  */
-private class ExitManager implements Runnable {
-    private staticfinal Vector threads;
+class ExitManager implements Runnable {
+    private static final Vector threads = new Vector();
 
     public static void addThread(Thread thread) {
         threads.add(thread);
@@ -39,17 +43,18 @@ private class ExitManager implements Runnable {
     }
 
     public void run() {
-        for (int thread = 0; thread < threads.size(); thread++) {
+        for (int i = 0; i < ExitManager.threads.size(); i++) {
+            Thread thread = (Thread) threads.get(i);
             thread.interrupt();
         }
-        System.out.printl("All threads interrupted");
+        System.out.println("All threads interrupted");
     }
 }
 
 /**
  * Responds to clients' requests
  */
-private class ServerRunnable implements Runnable {
+class ServerRunnable implements Runnable {
     // The port to listen on
     private int port;
 
@@ -72,37 +77,49 @@ private class ServerRunnable implements Runnable {
         ExitManager.addThread(Thread.currentThread());
 
         // Open server socket
+        DatagramSocket serverSocket = null;
         if (port > 0) {
             try {
-                DatagramSocket serverSocket = new DatagramSocket(port);
-            } catch (SocketException e) {
-                System.err.printl("Fatal: Failed to open socket with port "
-                    + port );
+                serverSocket = new DatagramSocket(port);
+            } catch (Exception e) {
+                System.err.println("Fatal: Failed to open socket with port " + port );
+                System.err.println("Exception: " + e);
                 System.exit(-1);
             }
         } else {
             try {
-                DatagramSocket serverSocket = new DatagramSocket();
-            } catch (SocketException e) {
-                System.err.printl("Fatal: Failed to open socket with any port");
+                serverSocket = new DatagramSocket();
+                port = serverSocket.getPort();
+            } catch (Exception e) {
+                System.err.println("Fatal: Failed to open socket with any port");
+                System.err.println("Exception: " + e);
                 System.exit(-1);
             }
-            port = serverSocket.getPort();
         }
 
-        // Only run until thread is interupted
-        while (Thread.currentThread().isInterupted() == false) {
-            Request request = Null;
+        // Only run until thread is interrupted
+        while (Thread.currentThread().isInterrupted() == false) {
+            Request request = null;
             // wait for a request
-            while (request == Null) {
-                request = getRequest(serverSocket);
+            while (request == null) {
+                try {
+                    request = getRequest(serverSocket);
+                } catch (Exception e) {
+                    System.err.println("Error: Caught Exception while trying to get request");
+                    System.err.println("Exception: " + e);
+                }
             }
 
-            serveRequest(request);
+            try {
+                serveRequest(request, serverSocket);
+            } catch (Exception e) {
+                System.err.println("Error: Caught Exception while trying to serve request");
+                System.err.println("Exception: " + e);
+            }
         }
 
         // Makes sure exit manager doesn't try to interup inactive thread
-        ExitManager.removeThread(Thread.currentThread);
+        ExitManager.removeThread(Thread.currentThread());
     }
 
     /**
@@ -110,7 +127,7 @@ private class ServerRunnable implements Runnable {
      *
      * Blocks until a request is received
      */
-    private Request getRequest(DatagramSocket socket) {
+    private Request getRequest(DatagramSocket socket) throws SocketException, IOException {
         // Get buffer size
         final int DGRAM_SIZE = socket.getReceiveBufferSize();
 
@@ -125,23 +142,46 @@ private class ServerRunnable implements Runnable {
         // build Request
         byte[] contents = new byte[packet.getLength()];
         System.arraycopy(buffer, packet.getOffset(), contents, 0, packet.getLength());
-        Request request = new Request(packet.getSocketAddress(), contents);
+        Request request = new Request((InetSocketAddress) packet.getSocketAddress(), contents);
 
         return request;
     }
 
     /**
-     * Respond to a request from a client
+     * Respond to a request from a client from socket.
      */
-    private void serveRequest(Request req) {
-        
+    private void serveRequest(Request req, DatagramSocket socket) throws SocketException {
+        String contents = null;
+        try {
+            contents = new String(req.getContents(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("Fatal: unsupported string encoding");
+            System.err.println("Exception: " + e);
+            System.exit(-2);
+        }
+        if (contents == "Hello Server") {
+            System.out.println("Got a greeting from a client at " + req.getSource());
+            System.out.println("Responding \"Hello Client at " + req.getSource() + "\"");
+
+            Message response = null;
+            try {
+                response = new Message("Hello Client at" + req.getSource(), socket, req.getSource());
+            } catch (UnsupportedEncodingException e) {
+                System.err.println("Fatal: unsupported string encoding");
+                System.err.println("Exception: " + e);
+                System.exit(-2);
+            }
+            response.sendMessage();
+        } else {
+            System.err.println("Got a bad request from a client at " + req.getSource());
+        }
     }
 }
 
 /**
  * Holds a request from a client
  */
-private class Request {
+class Request {
     private final InetSocketAddress source;
     private final byte[] contents;
 
@@ -162,7 +202,7 @@ private class Request {
 /**
  * Holds a message to send over UDP
  */
-private class Message {
+class Message {
     // The data to send
     private final byte[] contents;
 
@@ -172,5 +212,47 @@ private class Message {
     // The client to send the data to
     private final InetSocketAddress destination;
 
+    // The socket to send from
+    private final DatagramSocket socket;
+
+    public Message(byte[] contents, DatagramSocket socket, InetSocketAddress destination) throws SocketException {
+        this.contents = contents;
+        this.socket = socket;
+        this.destination = destination;
+        packets = new Vector();
+
+
+        // Get buffer size
+        final int DGRAM_SIZE = socket.getReceiveBufferSize();
+        final int HEADER_SIZE = 0;
+
+        // break contents into packets
+        for (int i = 0; i * (DGRAM_SIZE - HEADER_SIZE) < contents.length; i++) {
+            int length = DGRAM_SIZE - HEADER_SIZE;
+            if ((i + 1) * length >= contents.length) {
+                length = contents.length - i * length;
+            }
+            DatagramPacket packet = new DatagramPacket(contents,
+                                                       i * (DGRAM_SIZE - HEADER_SIZE),
+                                                       length,
+                                                       destination);
+            packets.add(packet);
+        }
+    }
+
+    public Message(String str, DatagramSocket socket, InetSocketAddress destination) throws UnsupportedEncodingException, SocketException {
+        this(str.getBytes("UTF-8"), socket, destination);
+    }
+
+    public void sendMessage() {
+        try {
+            for (int packet = 0; packet < packets.size(); packet++) {
+                socket.send((DatagramPacket) packets.get(packet));
+            }
+        } catch (Exception e) {
+            System.err.println("Error: Caught Exception while trying to send message");
+            System.err.println("Exception: " + e);
+        }
+    }
 
 }
