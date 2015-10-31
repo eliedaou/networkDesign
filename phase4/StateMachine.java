4d11be26798bb5d0af93425039408a9614e4f591
Elie Daou <elie.daou3@gmail.com>
2015-10-30 21:23:12 -0400

    created new folder for phase 4

---
+++ phase4/StateMachine.java
@@ -0,0 +1,18 @@
+public abstract class StateMachine {
+    protected interface State {}
+    protected interface Event {}
+
+
+    protected State currentState;
+    protected abstract State initialState();
+
+    public StateMachine() {
+        currentState = this.initialState();
+    }
+
+    protected abstract State delta(State currentState, Event event);
+
+    public void advance(Event event) {
+        currentState = this.delta(currentState, event);
+    }
+}
