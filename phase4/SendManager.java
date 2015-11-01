ce41da64f4fedfa7154e80828d7f73a03d90e754
Elie Daou <elie.daou3@gmail.com>
2015-11-01 12:25:42 -0500

    adjusted sendManager for next step

--- phase4/SendManager.java
+++ phase4/SendManager.java
@@ -5,157 +5,150 @@ import java.net.SocketException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class SendManager implements Runnable {
-    private final SendingStateMachine machine;
-    private final DatagramSocket socket;
-    private final FileInputStream fIn;
-    private final InetAddress remoteAddress;
-    private final int remotePort;
-    private Timer timer;
-    private final double ackError;
-    private final double dataError;
+	private final SendingStateMachine machine;
+	private final DatagramSocket socket;
+	private final FileInputStream fIn;
+	private final InetAddress remoteAddress;
+	private final int remotePort;
+	private final double ackError;
+	private final double dataError;
 
-    public SendManager(FileInputStream fIn, double ackError, double dataError) {
-        //temporary variable needed for try-catch
-        DatagramSocket tempSocket = null;
-        try {
-            //open socket on specified port
-            tempSocket = new DatagramSocket();
-            System.out.println("Opened socket on port " + tempSocket.getLocalPort());
-        } catch (IOException e) {
-            System.err.println("Fatal: exception caugth while opening socket");
-            System.err.println("\tException: " + e);
-            System.exit(-1);
-        }
-        socket = tempSocket;
-
-        //close socket when program exits
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            public void run() {
-                if (socket.isBound() && !socket.isClosed()) {
-                    socket.close();
-                    System.out.println("Closed socket");
-                }
-            }
-        }));
-
-        //set remoteAddress
-        InetAddress tempAddress = null;
-        try {
-            tempAddress = InetAddress.getByName("127.0.0.1");
-        } catch (UnknownHostException e) {
-            System.err.println("Fatal: exception caugth while resolving remote address");
-            System.err.println("\tException: " + e);
-            System.exit(-1);
-        }
-        remoteAddress = tempAddress;
-        remotePort = 10000;
-
-        //keep bit error variables
-        this.ackError = ackError;
-        this.dataError = dataError;
-        
-        //start state machine
-        machine = new SendingStateMachine(socket, remoteAddress, remotePort, ackError, dataError);
-
-        //keep file input stream
-        this.fIn = fIn;
-    }
-
-    class RemindTask extends TimerTask {
-    	DatagramPacket dataPacket;
-        ServerReceived packet;
-        SendingStateMachine.SendingEvent event;
-        byte[] receivedBuffer = new byte[1500];
-        
-        
-    	public RemindTask(byte[] receivedBuffer, SendingStateMachine.SendingEvent event) {
-        	this.receivedBuffer = receivedBuffer;
-        	this.event = event;
-        	
+	public SendManager(FileInputStream fIn, double ackError, double dataError) {
+		// temporary variable needed for try-catch
+		DatagramSocket tempSocket = null;
+		try {
+			// open socket on specified port
+			tempSocket = new DatagramSocket();
+			System.out.println("Opened socket on port "
+					+ tempSocket.getLocalPort());
+		} catch (IOException e) {
+			System.err.println("Fatal: exception caugth while opening socket");
+			System.err.println("\tException: " + e);
+			System.exit(-1);
 		}
-    	
-    	public void run() {
-        	//wait for ACK
-            while (machine.isWaitingForAck()) {
+		socket = tempSocket;
 
-            	//get ack from network
-                dataPacket = new DatagramPacket(receivedBuffer, receivedBuffer.length);
-                try {
-					socket.receive(dataPacket);
-				} catch (IOException e) {
-					e.printStackTrace();
+		// close socket when program exits
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			public void run() {
+				if (socket.isBound() && !socket.isClosed()) {
+					socket.close();
+					System.out.println("Closed socket");
 				}
+			}
+		}));
 
-                //build event
-                packet = new ServerReceived(dataPacket);
-                event = new SendingStateMachine.SendingEvent(packet);
+		// set remoteAddress
+		InetAddress tempAddress = null;
+		try {
+			tempAddress = InetAddress.getByName("127.0.0.1");
+		} catch (UnknownHostException e) {
+			System.err
+					.println("Fatal: exception caugth while resolving remote address");
+			System.err.println("\tException: " + e);
+			System.exit(-1);
+		}
+		remoteAddress = tempAddress;
+		remotePort = 10000;
 
-                //give event to the state machine
-                machine.advance(event);
-            }
-            timer.cancel(); //Terminate the timer thread
-        }
-    }
+		// keep bit error variables
+		this.ackError = ackError;
+		this.dataError = dataError;
 
-    public void Reminder(int seconds, byte[] receivedBuffer, SendingStateMachine.SendingEvent event) {
-       	timer = new Timer();
-        timer.schedule(new RemindTask(receivedBuffer, event), seconds);
+		// start state machine
+		machine = new SendingStateMachine(socket, remoteAddress, remotePort,
+				ackError, dataError);
+
+		// keep file input stream
+		this.fIn = fIn;
 	}
-    
-    public void run() {
-        DatagramPacket dataPacket;
-        ServerReceived packet;
-        SendingStateMachine.SendingEvent event;
-        byte[] sendBuffer = new byte[1024];
-        byte[] receivedBuffer = new byte[1500];
-        byte[] data;
-        int bytesRead;
-        long total = 0;
-        try {
-            while ((bytesRead = fIn.read(sendBuffer, 5, sendBuffer.length - 5)) != -1) {
-                //make data array of correct size
-                data = new byte[bytesRead + 5];
-                System.arraycopy(sendBuffer, 0, data, 0, data.length);
 
-                //make packet from file
-                packet = new ServerReceived(data, remoteAddress, remotePort);
+	public void run() {
 
-                //build an event
-                event = new SendingStateMachine.SendingEvent(packet);
+		DatagramPacket dataPacket;
+		ServerReceived packet;
+		SendingStateMachine.SendingEvent event;
+		byte[] sendBuffer = new byte[1024];
+		byte[] receivedBuffer = new byte[1500];
+		byte[] data;
+		int bytesRead;
+		long total = 0;
+		try {
+			while ((bytesRead = fIn.read(sendBuffer, 5, sendBuffer.length - 5)) != -1) {
+				// make data array of correct size
+				data = new byte[bytesRead + 5];
+				System.arraycopy(sendBuffer, 0, data, 0, data.length);
 
-                //give the event to the state machine
-                machine.advance(event);
+				// make packet from file
+				packet = new ServerReceived(data, remoteAddress, remotePort);
 
-                // Timer, first digit is milliseconds
-                Reminder(5, receivedBuffer, event);
+				// build an event
+				event = new SendingStateMachine.SendingEvent(packet);
 
-                total += bytesRead;
-                //System.out.println(total + "bytes sent");
-            }
-        } catch (IOException e) {
-            System.err.println("Fatal: exception caugth while reading file");
-            System.err.println("\tException: " + e);
-            System.exit(-1);
-        }
+				// give the event to the state machine
+				machine.advance(event);
 
-        //send final packet 100 times for safety
-        data = new byte[5];
-        data[0] = -1;
-        data[4] = -1;
+				// wait for ACK
+				WaitForAck();
 
-        for (int i = 0; i < 100; ++i) {
-            dataPacket = new DatagramPacket(data, data.length, remoteAddress, remotePort);
-            try {
-                socket.send(dataPacket);
-            } catch (IOException e) {
-                System.err.println("Fatal: exception caugth while sending final packet");
-                System.err.println("\tException: " + e);
-                System.exit(-1);
-            }
-        }
-    }
+				total += bytesRead;
+				// System.out.println(total + "bytes sent");
+			}
+		} catch (IOException e) {
+			System.err.println("Fatal: exception caugth while reading file");
+			System.err.println("\tException: " + e);
+			System.exit(-1);
+		}
+
+		// send final packet 100 times for safety
+		data = new byte[5];
+		data[0] = -1;
+		data[4] = -1;
+
+		for (int i = 0; i < 100; ++i) {
+			dataPacket = new DatagramPacket(data, data.length, remoteAddress,
+					remotePort);
+			try {
+				socket.send(dataPacket);
+			} catch (IOException e) {
+				System.err
+						.println("Fatal: exception caugth while sending final packet");
+				System.err.println("\tException: " + e);
+				System.exit(-1);
+			}
+		}
+	}
+
+	public void WaitForAck() throws IOException {
+
+		DatagramPacket dataPacket = null;
+		ServerReceived packet;
+
+		SendingStateMachine.SendingEvent event;
+		byte[] sendBuffer = new byte[1024];
+		byte[] receivedBuffer = new byte[1500];
+		long startTime = System.currentTimeMillis();
+		long endTime = startTime + 500L;
+
+		while (machine.isWaitingForAck() && (System.currentTimeMillis() < endTime)) {
+			// get ack from network
+			dataPacket = new DatagramPacket(receivedBuffer,
+					receivedBuffer.length);
+			socket.setSoTimeout(500);
+			socket.receive(dataPacket);
+
+			if (dataPacket != null) {
+				// build event
+				packet = new ServerReceived(dataPacket);
+				event = new SendingStateMachine.SendingEvent(packet);
+				
+				// give event to the state machine
+				machine.advance(event);
+			}
+			
+		}
+	}
+
 }
