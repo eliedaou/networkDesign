cb8d5c8110946d52c7159c083030bc3015bb6c6d
Elie Daou <elie.daou3@gmail.com>
2015-11-26 01:06:28 -0500

    implementing ack error and loss

--- phase5/src/daoumoyer/sender/ReceivedAck.java
+++ phase5/src/daoumoyer/sender/ReceivedAck.java
@@ -1,6 +1,7 @@
 package daoumoyer.sender;
 
 import java.net.DatagramPacket;
+import java.lang.Math;
 
 /**
  * @author Grant Moyer
@@ -16,22 +17,33 @@ public class ReceivedAck {
 		byte[] bytes = packet.getData();
 		this.ackError = ackError;
 		this.ackLoss = ackLoss;
+		double checkIfBitError = Math.random();
+		double checkIfAckLoss = Math.random();
 		
-		//get checksum in big endian format
-		int o = packet.getOffset();
-		long check = 0;
-		for (int i = 0; i < 8; ++i) {
-			check += ((long) bytes[o + i] & 0xff) << (8 - 1 - i)*8;
+		// if it's greater than the ratio, than the ack was received
+		if (checkIfAckLoss > this.ackLoss) {
+			
+			//get checksum in big endian format
+			int o = packet.getOffset();
+			long check = 0;
+			for (int i = 0; i < 8; ++i) {
+				if (checkIfBitError < this.ackError) {
+					//this is where we would change the bit because it is in the ratio
+				}
+				check += ((long) bytes[o + i] & 0xff) << (8 - 1 - i)*8;
+			}
+	
+			//get sequence number in big endian format
+			o += 8;
+			seqNum = 0;
+			for (int i = 0; i < 8; ++i) {
+				seqNum += ((long) bytes[o + i] & 0xff) << (8 - 1 - i)*8;
+			}
+	
+			corrupt = seqNum != check;
 		}
-
-		//get sequence number in big endian format
-		o += 8;
-		seqNum = 0;
-		for (int i = 0; i < 8; ++i) {
-			seqNum += ((long) bytes[o + i] & 0xff) << (8 - 1 - i)*8;
-		}
-
-		corrupt = seqNum != check;
+		//if it's less than or equal to the ratio, then the ack was dropped, but i'm not sure how to represent that
+		// i think we can just omit it
 	}
 
 
